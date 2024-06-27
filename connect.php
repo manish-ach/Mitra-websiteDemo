95773d25807e3416a5d3731c87ea08ea0ec7c7ba
Manish Acharya <manuacharya54@gmail.com>
2024-06-28 00:06:36 +0545

    changes done

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Replace with our actual data file (if it exists)
-$data = json_decode(file_get_contents('data.json'), true);
+$data = json_decode(file_get_contents('register/data.json'), true);
 
 // Check if user exists
 if (isset($data[$username]) && $data[$username]['password'] === $password) {
---
+++ register/register.php
@@ -0,0 +1,20 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+
+$data = array(
+  $username => array(
+    'password' => $password,
+    'email' => $email
+  )
+);
+
+// Write data to JSON file (ensure correct path and permission)
+$jsonData = json_encode($data);
+file_put_contents('data.json', $jsonData);
+
+echo "Data written to data.json!";
+?>
