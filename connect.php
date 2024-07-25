8193cc9b21322a34c68b9fce398e3310ed39f8d9
Manish Acharya <manuacharya54@gmail.com>
2024-07-25 08:52:35 +0545

    error fixing

--- connect.php
+++ connect.php
@@ -8,8 +8,9 @@ $data = json_decode(file_get_contents('register/data.json'), true);
 
 // Check if user exists
 if (isset($data[$username]) && $data[$username]['password'] === $password) {
-  echo "Login failed!";
-  header("Location: register/register.html");
+  echo "Login successful!";
+  header("Location: main/home/home.html");
+  exit();
 } else {
   echo "Login failed!";
   header("Location: ../login.html");
